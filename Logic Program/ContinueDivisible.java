901365c7d8ac85c872a5a70872a9501d085bc4ef
Sakshi_Nikhate <115627152+Sakshi-Nikhate@users.noreply.github.com>
2025-01-09 17:27:33 +0530

    .

--- Logic Program/ContinueDivisible.java
+++ Logic Program/ContinueDivisible.java
@@ -2,7 +2,7 @@ public class ContinueDivisible {
     public static void main(String[] args) {
         for(int i=1; i<=100; i++)
         {
-            if(i%5==0)
+            if(i%10==0)
             {
                 continue;
             }
