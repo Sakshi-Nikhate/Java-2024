0b6c5d51a3ad04ad0b61c9362f3c0a8ddfaef0dd
Sakshi_Nikhate <115627152+Sakshi-Nikhate@users.noreply.github.com>
2025-01-15 21:20:12 +0530

    .

--- Logic Program/EvenOdd.java
+++ Logic Program/EvenOdd.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
  class EvenOdd{
      	public static void main(String[] args){
      	 Scanner sc = new Scanner (System.in);
-     	 int number = 100 ;
+     	 int number = 50 ;
       if (number % 2 == 0) {
   System.out.println(number + " is even.");
 } else {
