2b2c7ac60a46fcdecf0d8c893a418edd08c6f630
Sakshi_Nikhate <115627152+Sakshi-Nikhate@users.noreply.github.com>
2025-01-20 23:32:19 +0530

    .

--- Logic Program/Loop.java
+++ Logic Program/Loop.java
@@ -2,7 +2,7 @@ class Loop
 {
     public static void main(String[] args)
     {
-        for(int i= 1 ; i<=50;i++)
+        for(int i= 1 ; i<=100;i++)
         {
             System.out.println(i+" ");
         }
