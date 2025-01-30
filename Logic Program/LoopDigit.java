c9e13190f2e671909b973298afd90034bc1e612f
Sakshi_Nikhate <115627152+Sakshi-Nikhate@users.noreply.github.com>
2025-01-30 23:55:08 +0530

    .

--- Logic Program/LoopDigit.java
+++ Logic Program/LoopDigit.java
@@ -1,6 +1,6 @@
 public class LoopDigit {
     public static void main(String[] args) {
-        for(int i=0;i<=25;i++)
+        for(int i=0;i<=5;i++)
         {
              System.out.println(i+" ");
         }
