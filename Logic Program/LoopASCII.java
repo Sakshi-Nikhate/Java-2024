d5a65a175ed477f1ecac5cfc96a3086b4bf8c5bd
Sakshi_Nikhate <115627152+Sakshi-Nikhate@users.noreply.github.com>
2025-02-06 23:53:31 +0530

    .

--- Logic Program/LoopASCII.java
+++ Logic Program/LoopASCII.java
@@ -1,6 +1,6 @@
 public class LoopASCII {
     public static void main(String[] args) {
-        for(int i=65; i<=90 ; i++)
+        for(int i=65; i<=80 ; i++)
         {
             System.out.println((char)i + ":" + i+ " ");
         }
